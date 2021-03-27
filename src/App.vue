44c4bc35a5488b663e71fb000dec13d84328b182
kunwoo park <scv74502@gmail.com>
2021-03-28 03:23:57 +0900

    done til lec.17

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,24 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+    app
+    color="primary"
+    dark
     >
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <site-title :title="title"></site-title>
+      <site-title :title="site.title"></site-title>
       <v-spacer/>
       <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
       <v-btn icon @click="read"><v-icon>mdi-numeric</v-icon></v-btn>
       <v-btn icon @click="readOne"><v-icon>mdi-account-badge</v-icon></v-btn>
     </v-app-bar>
      <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <site-menu></site-menu>
+       <site-menu :items="site.menu"></site-menu>
     </v-navigation-drawer>
     <v-content>
-    <site-footer :footer="footer"></site-footer>
       <router-view/>
     </v-content>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
@@ -44,18 +33,34 @@ export default {
   data () {
     return {
       drawer: false,
-      itmes: [],
-      title: '나의 타이틀입니다',
-      footer: '푸터입니다'
+      sites: {
+        menu: [],
+        title: '나의 타이틀입니다',
+        footer: '푸터입니다'
+      }
     }
+  },
+  created () {
+    this.subscribe()
   },
   mounted () {
     console.log(this.$firebase)
   },
   methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+        }
+        this.site = v
+      }, (e) => {
+        console.log(e.message)
+      })
+    },
     save () {
       console.log('save@@@')
-      this.$firebase.database().ref().child('abcd').set({
+      this.$firebase.database().ref().child('abcd').child('abcd').child('abcd').set({
         title: 'abcd', text: 'tttttt'
       })
     },
